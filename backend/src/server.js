fd9e10b58821ad666d8544c3ae3e4a56c8c4bee1
Lucy Zhang <luciazhang890@gmail.com>
2025-07-04 19:18:59 +0930

    Restructure: separate routing and controller logic

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import noteRoutes from "./routes/notesRoutes.js";
+
+const app = express();
+
+app.use("/api/notes", noteRoutes);
+
+
+
+app.listen(5001, () => {
+    console.log("Server is running on port 5001");
+})
