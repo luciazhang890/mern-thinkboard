a463b3683d956d9fab8b7872e62b62a8b6a2eb7d
Lucy Zhang <luciazhang890@gmail.com>
2025-07-06 12:37:45 +0930

    set up MongoDB connection and created Note model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,23 @@
 import express from "express";
 import noteRoutes from "./routes/notesRoutes.js";
+import {connectDB} from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", noteRoutes);
 
 
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+    console.log("Server started on PORT", PORT);
 })
+
+
