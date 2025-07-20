7ae77b6d328ea946e472f990d5692368089c0897
Lucy Zhang <luciazhang890@gmail.com>
2025-07-20 22:25:44 +0930

    finish Create Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
 import { useState } from 'react'
 import { useEffect } from 'react';
-import axios from 'axios';
+import api from '../lib/axios';
 import toast from "react-hot-toast";
 import NoteCard from '../components/NoteCard';
 
@@ -15,7 +15,7 @@ const HomePage = () => {
     useEffect(() => {
         const fetchNotes = async () => {
             try {
-                const res = await axios.get("http://localhost:5001/api/notes")
+                const res = await api.get("/notes")
                 console.log(res.data)
                 setNotes(res.data)
                 setIsRateLimited(false)
